f0b3bd162b76102c37dc6d161e9b9d5e12a42bb2
jonghyun oh <127705814+dydtn4814@users.noreply.github.com>
2025-05-23 11:32:49 +0900

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,9 +6,8 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('kakao/login/', views.login_kakao, name='kakao_login'),
-    path('kakao/callback/', views.kakao_callback, name='kakao_callback'),
+  
     path('kakao/webhook/', views.kakao_webhook, name='kakao_webhook'),
-    path('kakao/send_message/', views.send_kakao_message, name='kakao_send_message'),
+   
     path("sensor/", views.sensor_data),
 ]
